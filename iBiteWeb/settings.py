9becf7037acfcc9cf27a573410e63af6915511d8
Jaward Sesay <jkef96@icloud.com>
2022-04-01 15:01:38 +0800

    Update settings.py

--- iBiteWeb/settings.py
+++ iBiteWeb/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = '2jek@8e$aes+^$gkmm32j1fbr60s!xbm0rxrgd+$c^esrakx!$'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['ibite.herokuapp.com', '127.0.0.1']
 
